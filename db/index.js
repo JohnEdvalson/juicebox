d1870e31fff073ebbd7832598543b0e095feab12
SMan91 <stephan.manley@yahoo.com>
2022-07-14 14:30:25 -0600

    Doing the heroku stuff

--- db/index.js
+++ db/index.js
@@ -3,7 +3,9 @@
 const { Client } = require("pg"); // imports the pg module
 
 // supply the db name and location of the database
-const client = new Client("postgres://localhost:5432/juicebox-dev");
+const client = new Client(
+  process.env.DATABASE_URL || "postgres://localhost:5432/juicebox-dev"
+);
 
 // in db/index.js
 
